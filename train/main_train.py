3529a110178be2d1fb143f89d67d97a09609adb1
seyed <seyedamanzargari@gmail.com>
2022-05-07 00:51:28 +0430

    add pre trained weight in train

--- train/main_train.py
+++ train/main_train.py
@@ -4,13 +4,13 @@ from flask import Flask, request
 from utils.utils import dict_to_json, mkdir_p, movefiles, jsonfile2dict
 import json
 from utils.yaml_file import dict_to_yaml
-from preimutils.preimutils.object_detection.yolo import AMRLImageAug
-from preimutils.preimutils.object_detection.yolo.coco2yolo import COCO2YOLO
-from preimutils.preimutils.object_detection.yolo.train_validation_sep import separate_test_val
+from preimutils.object_detection.yolo import AMRLImageAug
+from preimutils.object_detection.yolo.coco2yolo import COCO2YOLO
+from preimutils.object_detection.yolo.train_validation_sep import separate_test_val
 from yolov5 import train
 import requests
 
-normpth = os.path.normpath
+normpath = os.path.normpath
 
 app = Flask(__name__)
 TRAIN_URL = "/"
@@ -25,6 +25,7 @@ def train_model():
     is_augment = req["is_augment"]
     validation_split = req["validation_split"]
     data_type = req["data_type"]
+    pre_trained_path = req.get("pretrained_path", '')
 
     try:
         image_size = req["image_size"]
@@ -40,13 +41,13 @@ def train_model():
     mkdir_p('tmp')
     # make raw Label Folder
     os.makedirs(os.path.join('tmp/DATASET/raw_annotations/'))
-    image_path = normpth(image_path)
+    image_path = normpath(image_path)
 
     if data_type == "coco":
         # Save coco file in temp folder
         dict_to_json(data, os.path.join('tmp', 'coco.json'))
         # Load coco.json file
-        c2y = COCO2YOLO(jsonfile2dict(normpth('tmp/coco.json')), normpth('tmp/DATASET/raw_annotations'))
+        c2y = COCO2YOLO(jsonfile2dict(normpath('tmp/coco.json')), normpath('tmp/DATASET/raw_annotations'))
         # Save Classes in coco_classes.txt
         c2y.save_classes(os.path.join('tmp/DATASET/'))
 
@@ -73,32 +74,32 @@ def train_model():
         os.makedirs('tmp/DATASET/raw_images')
         print(image_path, label_path)
         for name in os.listdir(image_path):
-            shutil.copyfile(os.path.join(image_path, name), os.path.join(normpth('tmp/DATASET/raw_images'), name))
+            shutil.copyfile(os.path.join(image_path, name), os.path.join(normpath('tmp/DATASET/raw_images'), name))
 
-        for name in os.listdir(normpth(label_path)):
-            shutil.copyfile(os.path.join(label_path, name), os.path.join(normpth('tmp/DATASET/raw_annotations'), name))
+        for name in os.listdir(normpath(label_path)):
+            shutil.copyfile(os.path.join(label_path, name), os.path.join(normpath('tmp/DATASET/raw_annotations'), name))
 
     # Train test split
     separate_test_val(
-        images_dir=normpth('tmp/DATASET/raw_images'),
-        txts_dir=normpth('tmp/DATASET/raw_annotations'),
-        dst_validatoion_dir=normpth('tmp/DATASET/yolo_validation'),
-        dst_train_dir=normpth('tmp/DATASET/yolo_train'),
+        images_dir=normpath('tmp/DATASET/raw_images'),
+        txts_dir=normpath('tmp/DATASET/raw_annotations'),
+        dst_validatoion_dir=normpath('tmp/DATASET/yolo_validation'),
+        dst_train_dir=normpath('tmp/DATASET/yolo_train'),
         validation_percentage=validation_split
     )
 
     if is_augment:
         aug_params = req["augment_params"]
         # Augment train Data
-        aug = AMRLImageAug(normpth('tmp/DATASET/yolo_train/annotations'),
-                           normpth('tmp/DATASET/yolo_train/images'),
-                           normpth('tmp/DATASET/yolo_train_augmented'))
+        aug = AMRLImageAug(normpath('tmp/DATASET/yolo_train/annotations'),
+                           normpath('tmp/DATASET/yolo_train/images'),
+                           normpath('tmp/DATASET/yolo_train_augmented'))
         aug.auto_augmentation(**aug_params)
 
         # Augment validation Data
-        aug = AMRLImageAug(normpth('tmp/DATASET/yolo_validation/annotations'),
-                           normpth('tmp/DATASET/yolo_validation/images'),
-                           normpth('tmp/DATASET/yolo_validation_augmented'))
+        aug = AMRLImageAug(normpath('tmp/DATASET/yolo_validation/annotations'),
+                           normpath('tmp/DATASET/yolo_validation/images'),
+                           normpath('tmp/DATASET/yolo_validation_augmented'))
         aug.auto_augmentation(**aug_params)
 
         # Change directory to train yolo
@@ -107,12 +108,12 @@ def train_model():
         os.makedirs('tmp/DATASET/yolo_data/labels/train')
         os.makedirs('tmp/DATASET/yolo_data/labels/val')
 
-        movefiles(normpth('tmp/DATASET/yolo_validation_augmented/annotations'),
-                  normpth('tmp/DATASET/yolo_data/labels/val'))
-        movefiles(normpth('tmp/DATASET/yolo_validation_augmented/images'), normpth('tmp/DATASET/yolo_data/images/val'))
-        movefiles(normpth('tmp/DATASET/yolo_train_augmented/annotations'),
-                  normpth('tmp/DATASET/yolo_data/labels/train'))
-        movefiles(normpth('tmp/DATASET/yolo_train_augmented/images'), normpth('tmp/DATASET/yolo_data/images/train'))
+        movefiles(normpath('tmp/DATASET/yolo_validation_augmented/annotations'),
+                  normpath('tmp/DATASET/yolo_data/labels/val'))
+        movefiles(normpath('tmp/DATASET/yolo_validation_augmented/images'), normpath('tmp/DATASET/yolo_data/images/val'))
+        movefiles(normpath('tmp/DATASET/yolo_train_augmented/annotations'),
+                  normpath('tmp/DATASET/yolo_data/labels/train'))
+        movefiles(normpath('tmp/DATASET/yolo_train_augmented/images'), normpath('tmp/DATASET/yolo_data/images/train'))
 
     else:
         # Change directory to train yolo
@@ -121,17 +122,17 @@ def train_model():
         os.makedirs('tmp/DATASET/yolo_data/labels/train')
         os.makedirs('tmp/DATASET/yolo_data/labels/val')
 
-        movefiles(normpth('tmp/DATASET/yolo_validation/annotations/'),
-                  normpth('tmp/DATASET/yolo_data/labels/val/'))
+        movefiles(normpath('tmp/DATASET/yolo_validation/annotations/'),
+                  normpath('tmp/DATASET/yolo_data/labels/val/'))
 
-        movefiles(normpth('tmp/DATASET/yolo_validation/images/'),
-                  normpth('tmp/DATASET/yolo_data/images/val/'))
+        movefiles(normpath('tmp/DATASET/yolo_validation/images/'),
+                  normpath('tmp/DATASET/yolo_data/images/val/'))
 
-        movefiles(normpth('tmp/DATASET/yolo_train/annotations/'),
-                  normpth('tmp/DATASET/yolo_data/labels/train/'))
+        movefiles(normpath('tmp/DATASET/yolo_train/annotations/'),
+                  normpath('tmp/DATASET/yolo_data/labels/train/'))
 
-        movefiles(normpth('tmp/DATASET/yolo_train/images/'),
-                  normpth('tmp/DATASET/yolo_data/images/train/'))
+        movefiles(normpath('tmp/DATASET/yolo_train/images/'),
+                  normpath('tmp/DATASET/yolo_data/images/train/'))
 
     d = {
         'train': os.path.abspath('tmp/DATASET/yolo_data/images/train'),
@@ -140,9 +141,9 @@ def train_model():
         'names': classes
     }
     dict_to_yaml(d, 'tmp/data.yaml')
-    save_dir = normpth(req['save_dir'])
+    save_dir = normpath(req['save_dir'])
     # Training yolo
-    train.run(data='tmp/data.yaml', imgsz=image_size, weights=weight, save_dir=save_dir, log_url=Log_url)
+    train.run(data='tmp/data.yaml', imgsz=image_size, weights=weight, save_dir=save_dir, log_url=Log_url, weights=pre_trained_path)
 
     # delete temp file
     if os.path.exists('tmp') and os.path.isdir('tmp'):
