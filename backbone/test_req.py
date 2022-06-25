988474d88c35a57a187ddd202711b4c4c46455ce
Ali Amini Bagh <aliaminibagh@gmail.com>
2022-06-25 10:20:01 +0430

    feat(inference-request): get files from user to inference
    #8

--- backbone/test_req.py
+++ backbone/test_req.py
@@ -1,17 +1,18 @@
 import requests
 import pprint
 import cv2
-
+import sys
 
 
 DETECTION_URL = "http://localhost:5000/v1/object-detection/yolov5s/detect"
-TEST_IMAGE = "path/to/test/image.jpg"
 
-def send_with_image(image_path,detection_url):
+
+def send_with_image(image_path, detection_url):
     image_data = open(image_path, "rb").read()
     response = requests.post(detection_url, files={"image": image_data}).json()
     pprint.pprint(response)
     return response
+
 
 def send_cv2_image(cv2image, detection_url):
     cv2image = cv2.cvtColor(cv2image, cv2.COLOR_BGR2RGB)
@@ -21,5 +22,5 @@ def send_cv2_image(cv2image, detection_url):
 
 
 if __name__ == "__main__":
-    cv_image = cv2.imread(TEST_IMAGE)
-    send_cv2_image(cv_image)
+    cv_image = cv2.imread(sys.argv[1])
+    send_cv2_image(cv_image, DETECTION_URL)
