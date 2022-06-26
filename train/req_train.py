24df3374e1e57ce33591b2c32cf3f5dcad51f812
Ali Amini Bagh <aliaminibagh@gmail.com>
2022-06-26 18:28:58 +0430

    perf(train): edit request file to manage yolo and coco better
    #10

--- train/req_train.py
+++ train/req_train.py
@@ -1,13 +1,11 @@
 import json
 import requests
-
-# fisrt Copy train2.py in yolov5 folder
-# then run this script
+from pathlib import Path
 
 
-def train_yolo_data_without_augment():
+def train_yolo_data(is_augment):
     url = 'http://127.0.0.1:8000/train'  # server url
-    data = {'label': ['c1', 'c2'],
+    data = {'label': ['c1', 'c2'],  # one class
             'image_path': '/code/dataset/images',
             'label_path': '/code/dataset/labels',
             'image_size': 640,
@@ -15,81 +13,36 @@ def train_yolo_data_without_augment():
             'weight': 'yolov5n.pt',
             'validation_split': 0.2,
             'data_type': 'yolo',
-            'save_dir': 'results/'}
-    r = requests.post(url, json=data)
-    print(r.text)
-
-
-# function train yolo data with augment
-def train_yolo_data_with_augment():
-    url = 'http://127.0.0.1:8000/train'  # server url
-    data = {'label': ['c1', 'c2'],
-            'image_path': 'data/images',
-            'label_path': 'data/labels',
-            'image_size': 640,
-            'epochs': 7,
-            'weight': 'yolov5n.pt',
-            'validation_split': 0.2,
-            'data_type': 'yolo',
             'save_dir': 'results/',
-            'is_augment': True,
-            'augment_params': {'count_of_each': 2}}
+            'is_augment': is_augment}
     r = requests.post(url, json=data)
     print(r.text)
 
 
-def train_coco_data_without_augment():
+def train_coco_data(is_augment):
     url = 'http://127.0.0.1:8000/train'  # server url
 
-    with open('coco_annotation.json') as f:
+    path = str((Path(__file__).parent).parent) + \
+        '/volumes/dataset/labels/coco_annotation.json'
+    with open(path) as f:
         coco_annotation = json.load(f)
-        coco_annotation = json.dumps(coco_annotation)
-
     data = {'label': coco_annotation,
-            'image_path': 'image_path',
-            'label_path': 'label_path',
+            'image_path': '/code/dataset/images',
             'image_size': 640,
             'epochs': 7,
             'weight': 'yolov5n.pt',
             'validation_split': 0.1,
             'data_type': 'coco',
-            'save_dir': 'save_dir'}
-    r = requests.post(url, json=data)
-    print(r.text)
-
-
-def train_coco_data_with_augment():
-    url = 'http://127.0.0.1:8000/train'  # server url
-
-    with open('coco_annotation.json') as f:
-        coco_annotation = json.load(f)
-        coco_annotation = json.dumps(coco_annotation)
-
-    data = {'label': coco_annotation,
-            'image_path': 'image_path',
-            'label_path': 'label_path',
-            'image_size': 640,
-            'epochs': 7,
-            'weight': 'yolov5n.pt',
-            'validation_split': 0.1,
-            'data_type': 'coco',
-            'save_dir': 'save_dir',
-            'is_augment': True,
-            'augment_params': {'count_of_each': 2}}
+            'save_dir': 'results/',
+            'is_augment': is_augment}
     r = requests.post(url, json=data)
     print(r.text)
 
 
 if __name__ == '__main__':
-    data_type = 'yolo'  # 'yolo' or 'coco'
-    is_augment = False  # True or False
+    data_type = 'coco'  # 'yolo' or 'coco'
+    is_augment = True  # True or False
     if data_type == 'yolo':
-        if is_augment:
-            train_yolo_data_with_augment()  # train yolo data with augment
-        else:
-            train_yolo_data_without_augment()  # train yolo data without augment
+        train_yolo_data(is_augment)
     elif data_type == 'coco':
-        if is_augment:
-            train_coco_data_with_augment()  # train coco data with augment
-        else:
-            train_coco_data_without_augment()  # train coco data without augment
+        train_coco_data(is_augment)
