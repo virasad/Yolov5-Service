c3ec502150eed01a5841a5d0befe798766d394a8
Ali Amini Bagh <aliaminibagh@gmail.com>
2022-06-26 18:23:59 +0430

    refactor(inference): read best.pt from a path to inference with it
    #10

--- backbone/detect.py
+++ backbone/detect.py
@@ -5,11 +5,8 @@ import os
 class Detector():
     def __init__(self):
         self.model = torch.hub.load('ultralytics/yolov5', 'custom',
-                               path=os.environ.get('MODEL_PATH', 'weights/best.pt'))
-    def set_model(self, model_path):
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom',
-                               path=model_path)
-    
+                                    path=os.environ.get('MODEL_PATH', 'weights/best.pt'))
+
     def detect(self, img):
         """
         img: PIL image
